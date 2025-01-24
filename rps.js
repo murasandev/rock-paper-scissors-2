4382d08c6a52f05af31511ed3e97603900006914
Dylan M <dmurayama93@gmail.com>
2025-01-23 14:35:12 -1000

    adds a min variable to random number function to create more customizability

--- rps.js
+++ rps.js
@@ -1,12 +1,12 @@
-console.log(getRandomNumber(3))
+console.log(getRandomNumber(0, 3))
 
 // Generate random number from 1-3
 //Assign each value rock paper or scissor
 //Get input from user
 // increment winner score
 
-function getRandomNumber(max){
-    return Math.floor(Math.random() * max);
+function getRandomNumber(min, max){
+    return Math.floor(Math.random() * (max - min) + min);
 }
 
 function getComputerChoic(){
