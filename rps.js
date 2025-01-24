1ea3b0080002c008affcf84093feb9b1a67cb90b
Dylan M <dmurayama93@gmail.com>
2025-01-23 14:32:06 -1000

    creates random number generator from 0-3

--- rps.js
+++ rps.js
@@ -1,1 +1,14 @@
-console.log("Hello World")
+console.log(getRandomNumber(3))
+
+// Generate random number from 1-3
+//Assign each value rock paper or scissor
+//Get input from user
+// increment winner score
+
+function getRandomNumber(max){
+    return Math.floor(Math.random() * max);
+}
+
+function getComputerChoic(){
+
+}
