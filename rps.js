c9704ac36dfe52fc225aac569eb3cca720bdd067
Dylan M <dmurayama93@gmail.com>
2025-01-23 15:16:20 -1000

    creates get user input function

--- rps.js
+++ rps.js
@@ -1,14 +1,33 @@
-console.log(getRandomNumber(0, 3))
+let computerNumber;
+let userInput;
 
 // Generate random number from 1-3
 //Assign each value rock paper or scissor
 //Get input from user
 // increment winner score
+getUserInput();
+checkComputerChoice();
 
-function getRandomNumber(min, max){
-    return Math.floor(Math.random() * (max - min) + min);
+function calcRandomNumber(min, max){
+    computerNumber = Math.floor(Math.random() * (max - min) + min);
 }
 
-function getComputerChoic(){
+function checkComputerChoice(){
+    calcRandomNumber(0, 3);
 
+    switch(computerNumber){
+        case 0:
+            console.log(`Rock: ${computerNumber}`);
+            break;
+        case 1:
+            console.log(`Paper ${computerNumber}`);
+            break;
+        case 2:
+            console.log(`Scissors ${computerNumber}`);
+            break;
+    }
+}
+
+function getUserInput(){
+    userInput = prompt("Rock, Paper, or Scissors?","3, 2, 1! Shoot!");
 }
